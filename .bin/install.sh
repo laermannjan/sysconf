0448424883edc7f3809ae9123cf34a8331fef4fd
Jan Laermann <laermannjan@gmail.com>
2022-06-10 15:55:19 +0200

    update install script again

--- .bin/install.sh
+++ .bin/install.sh
@@ -14,7 +14,7 @@ git clone --bare git@github.com:laermannjan/dotfiles "$DOTFILES_DIR"
 
 mkdir -p "${DOTFILES_DIR}.old"
 
-dotfiles checkout || echo "Backing up pre-existing dotfiles." && dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I@ backup @ && dotfiles checkout
+dotfiles checkout || echo "Backing up pre-existing dotfiles." && dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I@ sh -c 'backup @' && dotfiles checkout
 
 echo " >> Dotfiles installed successfully."
 dotfiles config --local status.showUntrackedFiles no
