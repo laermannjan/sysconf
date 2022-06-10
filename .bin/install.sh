06ec9aa6141072323ad29946d97ffc54c29aaade
Jan Laermann <laermannjan@gmail.com>
2022-06-10 15:42:07 +0200

    update install script

--- .bin/install.sh
+++ .bin/install.sh
@@ -2,19 +2,14 @@
 
 DOTFILES_DIR=$HOME/.dotfiles
 
-alias dotfiles="git --git-dir=$DOTFILES_DIR --work-tree=$HOME"
-git clone --bare git@github.com:laermannjan/dotfiles $DOTFILES_DIR
+function dotfiles {
+    git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" "$@"
+}
+git clone --bare git@github.com:laermannjan/dotfiles "$DOTFILES_DIR"
 
-mkdir -p ${DOTFILES_DIR}.old
-dotfiles checkout
-if [ $? = 0 ]; then
-    echo " >> Dotfiles installed successfully.";
-else
-    echo " >> Backing up pre-existing dotfiles.";
-    dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I@ mv @ ${DOTFILES_DIR}.old/@
-fi;
-dotfiles checkout
-if [ $? = 0 ]; then
-    echo " >> Dotfiles installed successfully."
-fi;
+mkdir -p "${DOTFILES_DIR}.old"
+
+dotfiles checkout || echo " >> Backing up pre-existing dotfiles." && dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I@ mv @ ${DOTFILES_DIR}.old/@ && dotfiles checkout
+
+echo " >> Dotfiles installed successfully."
 dotfiles config --local status.showUntrackedFiles no
