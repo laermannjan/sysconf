0cd38c4251873c3f3f93c18f6317a11d408c4138
Jan Laermann <laermannjan@gmail.com>
2022-06-12 23:07:16 +0200

    fix install script some more

--- .bin/install.sh
+++ .bin/install.sh
@@ -1,47 +1,44 @@
 #!/usr/bin/env bash
 
+echo "Installing Jan's configuration to this system. Buckle up!"
+cd "$HOME" || echo "  ERROR: can't cd into $HOME" && exit 1
+
 # Install Homebrew
 brew_installed=1
-if ! which brew >& /dev/null;then
-  brew_installed=0
-  echo Homebrew is not installed!
-  echo Install now...
-  echo /bin/bash -c \"\$\(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh\)\"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-  ret=$?
-  if [ $ret -ne 0 ];then
-    echo Failed to install Homebrew... please check your environment
-    exit $ret
-  fi
-  echo
-
-  for path in /home/linuxbrew/.linuxbrew $HOME/.linuxbrew /opt/homebrew /usr/local;do
-    if [ -f "$path/bin" ];then
-      PATH="$path/bin/brew"
+echo -n "Installing Homebrew..."
+if which brew >& /dev/null;then
+    echo "already installed."
+else
+    brew_installed=0
+    echo /bin/bash -c \"\$\(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh\)\"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    ret=$?
+    if [ $ret -ne 0 ];then
+        echo "Failed. Please investigate"
+        exit $ret
     fi
-  done
+
+    for path in /home/linuxbrew/.linuxbrew $HOME/.linuxbrew /opt/homebrew /usr/local;do
+        if [ -f "$path/bin" ];then
+            PATH="$path/bin/brew"
+        fi
+    done
+    echo -n "Checking brew doctor..."
+    brew doctor
+    if [ $? -ne 0 ];then
+        exit 1
+    fi
+    echo "OK"
 fi
 
-# Install Brew-file
-echo
-echo Install Brew-file...
-brew install rcmdnk/file/brew-file
-
-# Check if homebrew got problems
-if [ $brew_installed -eq 0 ];then
-  brew doctor
-  if [ $? -ne 0 ];then
-    echo Check brew environment!
-    exit 1
-  fi
-fi
+echo -n "- Installing Brew-file..."
+brew install rcmdnk/file/brew-file && echo "Ok" || echo "Failed" && exit 1
 
 # Save any pre-existing dotfiles that are already in the git repo and install those from the repo in
 # their place
 export DOTFILES_DIR="${HOME}/.dotfiles"
 export BACKUP_DIR="${DOTFILES_DIR}.old"
 
-cd "$HOME" || exit
 
 function dotfiles {
     git --git-dir="$DOTFILES_DIR" --work-tree="$HOME" "$@"
@@ -53,16 +50,15 @@ function backup {
     mv "$1" "${BACKUP_DIR}/$1"
 }
 export -f backup
-git clone --bare git@github.com:laermannjan/dotfiles "$DOTFILES_DIR"
+echo "Cloning bare git repo into $DOTFILES_DIR..."
+git clone --bare git@github.com:laermannjan/dotfiles "$DOTFILES_DIR" && echo "OK" || echo "Failed" && exit 1
 
-dotfiles checkout 2>&1 | grep -E "^\t" | awk {'print $1'} | xargs -I{} bash -c 'backup "{}"'
-dotfiles checkout
-
-echo "Dotfiles installed successfully."
+echo "Checking out dotfiles into \$HOME..."
+dotfiles checkout 2>&1 | grep -E "^\t" | awk {'print $1'} | xargs -I{} bash -c 'backup "{}"' && dotfiles checkout && echo "Ok" || echo "Failed"
 dotfiles config --local status.showUntrackedFiles no
 
 # install applications and packages
-echo "Installing from Brewfile (this may take a _long_ while)"
+echo "Installing from Brewfile (this may take a _long_ while)..."
 brew file update || echo "Some Packages could not be installed via Homebrew, will continue setup anyways"
 
 # install fonts
@@ -76,7 +72,9 @@ else
 fi
 
 # install doom 
-echo "Installing Doom Emacs"
-[ -d "$HOME/.emacs.d" ] && echo "Backing up pre-existing emacs config" && mv "$HOME/.emacs.d" "$HOME/.emacs.d.old"
+echo "Installing Doom Emacs..."
+[ -d "$HOME/.emacs.d" ] && echo "Backing up pre-existing emacs config" && mv --no-target-directory "$HOME/.emacs.d" "$HOME/.emacs.d.old"
 git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.emacs.d
 $HOME/.emacs.d/bin/doom -y install
+
+echo "System configuration complete!"
