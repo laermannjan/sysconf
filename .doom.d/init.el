a50e88dc110ec7c035aaa8d8b4555d70f0262b1c
Jan Laermann <laermannjan@gmail.com>
2022-06-14 19:00:09 +0200

    make python use mypy, isort, black

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -162,8 +162,7 @@
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python            ; beautiful is better than ugly
-        +lsp
-        +pyright)
+        +lsp)
        ;;qt                ; the 'cutest' gui framework ever
        ;;racket            ; a DSL for DSLs
        ;;raku              ; the artist formerly known as perl6
