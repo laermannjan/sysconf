cec49282aead2549290308f1f80934dfd2bf1666
Jan Laermann <laermannjan@gmail.com>
2022-11-06 04:01:49 +0100

    fix pasting over things

--- lvim/.config/lvim/lua/lj/keymaps.lua
+++ lvim/.config/lvim/lua/lj/keymaps.lua
@@ -1,2 +1,3 @@
 lvim.leader = "space"
-lvim.keys.visual_mode["p"] = "_dP" -- don't copy what I overwrite with paste
+lvim.keys.visual_mode["p"] = '"_dP' -- don't copy what I overwrite with paste
+lvim.builtin.which_key.mappings['x'] = { "<cmd>BufferKill<CR>", "Close Buffer" }
