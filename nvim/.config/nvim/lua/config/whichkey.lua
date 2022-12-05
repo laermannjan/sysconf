05de61d3c4470d66800d4bbaa706f7735f623a07
Jan Laermann <laermannjan@gmail.com>
2022-12-05 23:56:21 +0100

    feat(nvim): add code keymaps

--- nvim/.config/nvim/lua/config/whichkey.lua
+++ nvim/.config/nvim/lua/config/whichkey.lua
@@ -110,8 +110,140 @@ local function normal_keymap()
    whichkey.register(keymap, opts)
 end
 
+local function visual_keymap()
+   local keymap = {
+      g = {
+         name = "Git",
+         y = {
+            "<cmd>lua require'gitlinker'.get_buf_range_url('v', {action_callback = require'gitlinker.actions'.open_in_browser})<cr>",
+            "Link",
+         },
+      },
+
+      r = {
+         name = "Refactor",
+         f = { [[<cmd>lua require('refactoring').refactor('Extract Function')<cr>]], "Extract Function" },
+         F = {
+            [[ <cmd>lua require('refactoring').refactor('Extract Function to File')<cr>]],
+            "Extract Function to File",
+         },
+         v = { [[<cmd>lua require('refactoring').refactor('Extract Variable')<cr>]], "Extract Variable" },
+         i = { [[<cmd>lua require('refactoring').refactor('Inline Variable')<cr>]], "Inline Variable" },
+         r = { [[<cmd>lua require('telescope').extensions.refactoring.refactors()<cr>]], "Refactor" },
+         d = { [[<cmd>lua require('refactoring').debug.print_var({})<cr>]], "Debug Print Var" },
+      },
+   }
+
+   whichkey.register(keymap, v_opts)
+end
+
+local function code_keymap()
+   vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+         vim.schedule(CodeRunner)
+      end,
+   })
+
+   function CodeRunner()
+      local bufnr = vim.api.nvim_get_current_buf()
+      local ft = vim.api.nvim_buf_get_option(bufnr, "filetype")
+      local fname = vim.fn.expand "%:p:t"
+      local keymap_c = {} -- normal key map
+      local keymap_c_v = {} -- visual key map
+
+      if ft == "python" then
+         keymap_c = {
+            name = "Code",
+            i = { "<cmd>cexpr system('refurb --quiet ' . shellescape(expand('%'))) | copen<cr>", "Inspect" },
+            r = {
+               "<cmd>update<cr><cmd>lua require('utils.term').open_term([[python3 ]] .. vim.fn.shellescape(vim.fn.getreg('%'), 1), {direction = 'float'})<cr>",
+               "run file",
+            },
+            R = {
+               "<cmd>update<cr><cmd>lua require('utils.term').open_term([[python3 ]] .. vim.fn.shellescape(vim.fn.getreg('%'), 1) .. ' ' .. vim.fn.input('args: '), {direction = 'float'})<cr>",
+               "run file with args",
+            },
+            m = { "<cmd>TermExec cmd='nodemon -e py %'<cr>", "Monitor" },
+         }
+      elseif ft == "lua" then
+         keymap_c = {
+            name = "Code",
+            r = { "<cmd>luafile %<cr>", "run file" },
+         }
+      elseif ft == "rust" then
+         keymap_c = {
+            name = "Code",
+            r = { "<cmd>execute 'Cargo run' | startinsert<cr>", "run file" },
+            D = { "<cmd>RustDebuggables<cr>", "Debuggables" },
+            h = { "<cmd>RustHoverActions<cr>", "Hover Actions" },
+            R = { "<cmd>RustRunnables<cr>", "Runnables" },
+         }
+      elseif ft == "go" then
+         keymap_c = {
+            name = "Code",
+            r = { "<cmd>GoRun<cr>", "run file" },
+         }
+      elseif ft == "typescript" or ft == "typescriptreact" or ft == "javascript" or ft == "javascriptreact" then
+         keymap_c = {
+            name = "Code",
+            o = { "<cmd>TypescriptOrganizeImports<cr>", "Organize Imports" },
+            r = { "<cmd>TypescriptRenameFile<cr>", "Rename File" },
+            i = { "<cmd>TypescriptAddMissingImports<cr>", "Import Missing" },
+            F = { "<cmd>TypescriptFixAll<cr>", "Fix All" },
+            u = { "<cmd>TypescriptRemoveUnused<cr>", "Remove Unused" },
+            R = { "<cmd>lua require('config.test').javascript_runner()<cr>", "Choose Test Runner" },
+            -- s = { "<cmd>2TermExec cmd='yarn start'<cr>", "Yarn Start" },
+            -- t = { "<cmd>2TermExec cmd='yarn test'<cr>", "Yarn Test" },
+         }
+      elseif ft == "java" then
+         keymap_c = {
+            name = "Code",
+            o = { "<cmd>lua require'jdtls'.organize_imports()<cr>", "Organize Imports" },
+            v = { "<cmd>lua require('jdtls').extract_variable()<cr>", "Extract Variable" },
+            c = { "<cmd>lua require('jdtls').extract_constant()<cr>", "Extract Constant" },
+            t = { "<cmd>lua require('jdtls').test_class()<cr>", "Test Class" },
+            n = { "<cmd>lua require('jdtls').test_nearest_method()<cr>", "Test Nearest Method" },
+         }
+         keymap_c_v = {
+            name = "Code",
+            v = { "<cmd>lua require('jdtls').extract_variable(true)<cr>", "Extract Variable" },
+            c = { "<cmd>lua require('jdtls').extract_constant(true)<cr>", "Extract Constant" },
+            m = { "<cmd>lua require('jdtls').extract_method(true)<cr>", "Extract Method" },
+         }
+      end
+
+      if fname == "package.json" then
+         keymap_c.v = { "<cmd>lua require('package-info').show()<cr>", "Show Version" }
+         keymap_c.c = { "<cmd>lua require('package-info').change_version()<cr>", "Change Version" }
+         -- keymap_c.s = { "<cmd>2TermExec cmd='yarn start'<cr>", "Yarn Start" }
+         -- keymap_c.t = { "<cmd>2TermExec cmd='yarn test'<cr>", "Yarn Test" }
+      end
+
+      if fname == "Cargo.toml" then
+         keymap_c.u = { "<cmd>lua require('crates').upgrade_all_crates()<cr>", "Upgrade All Crates" }
+      end
+
+      if next(keymap_c) ~= nil then
+         local k = { c = keymap_c }
+         local o = { mode = "n", silent = true, noremap = true, buffer = bufnr, prefix = "<leader>", nowait = true }
+         whichkey.register(k, o)
+         -- legendary.bind_whichkey(k, o, false)
+      end
+
+      if next(keymap_c_v) ~= nil then
+         local k = { c = keymap_c_v }
+         local o = { mode = "v", silent = true, noremap = true, buffer = bufnr, prefix = "<leader>", nowait = true }
+         whichkey.register(k, o)
+      end
+
+   end
+end
+
 M.setup = function()
    normal_keymap()
+   visual_keymap()
+   code_keymap()
 end
 
 return M
--- nvim/.config/nvim/lua/utils/term.lua
+++ nvim/.config/nvim/lua/utils/term.lua
@@ -29,4 +29,29 @@ function M.docker_client_toggle()
    docker_client:toggle()
 end
 
+-- Open a terminal
+local function default_on_open(term)
+   vim.cmd "stopinsert"
+   vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+end
+
+function M.open_term(cmd, opts)
+   opts = opts or {}
+   opts.size = opts.size or vim.o.columns * 0.5
+   opts.direction = opts.direction or "vertical"
+   opts.on_open = opts.on_open or default_on_open
+   opts.on_exit = opts.on_exit or nil
+
+   local new_term = Terminal:new {
+      cmd = cmd,
+      dir = "git_dir",
+      auto_scroll = false,
+      close_on_exit = false,
+      start_in_insert = false,
+      on_open = opts.on_open,
+      on_exit = opts.on_exit,
+   }
+   new_term:open(opts.size, opts.direction)
+end
+
 return M
