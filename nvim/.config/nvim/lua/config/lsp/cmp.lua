4663957964f7f5be33181f59c522f472f4329412
Jan Laermann <laermannjan@gmail.com>
2022-12-06 11:27:37 +0100

    feat(nvim): add creates cmp source

--- nvim/.config/nvim/lua/config/lsp/cmp.lua
+++ nvim/.config/nvim/lua/config/lsp/cmp.lua
@@ -82,6 +82,7 @@ M._get_sources = function()
       { name = 'nvim_lsp', keyword_length = 3 },
       { name = 'buffer', keyword_length = 3 },
       { name = 'luasnip', keyword_length = 3 },
+      { name = "crates" },
    }
    return sources
 end
