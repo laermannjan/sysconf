23905ae34e77c505d67395fe5443394ac14faefc
Jan Laermann <laermannjan@gmail.com>
2022-12-06 13:26:45 +0100

    feat(nvim): add neoclip and telescope-dap

--- nvim/.config/nvim/lua/config/telescope.lua
+++ nvim/.config/nvim/lua/config/telescope.lua
@@ -27,6 +27,9 @@ M.setup = function()
    -- telescope.load_extension "project" -- telescope-project
    telescope.load_extension "projects" -- project.nvim
    telescope.load_extension "aerial"
+   telescope.load_extension "dap"
 
+   require('neoclip').setup() -- must be here, see: https://github.com/AckslD/nvim-neoclip.lua/issues/5
+   telescope.load_extension "neoclip"
 end
 return M
