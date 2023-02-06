7c07530b3bb95ec514651bb4600eca85da85c61a
Jan Laermann <laermannjan@gmail.com>
2023-02-06 08:54:43 +0200

    chore(lvim): cleanup

--- lvim/.config/lvim/lua/lj/cmp.lua
+++ lvim/.config/lvim/lua/lj/cmp.lua
@@ -1,6 +1,6 @@
 lvim.builtin.cmp.cmdline.enable = false
 lvim.builtin.cmp.window.documentation = true
 lvim.builtin.cmp.window.completion = {
-  border = "rounded",
-  winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
+   border = "rounded",
+   winhighlight = "NormalFloat:Pmenu,NormalFloat:Pmenu,CursorLine:PmenuSel,Search:None",
 }
