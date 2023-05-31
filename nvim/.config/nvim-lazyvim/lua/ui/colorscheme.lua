db0062cfadbd4d0d86b275b7bac1db2edb33991b
Jan Laermann <laermannjan@gmail.com>
2023-05-31 19:44:42 +0200

    feat(nvim): add basic lazyvim setup

---
+++ nvim/.config/nvim-lazyvim/lua/ui/colorscheme.lua
@@ -0,0 +1,9 @@
+return {
+  { "folke/tokyonight.nvim", name = "tokyonight", opts = { style = "night" } },
+  { "rose-pine/neovim", name = "rose-pine", opts = { variant = "auto" } }, -- moon
+  { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
+  { "catppuccin/nvim", name = "catppuccin", opts = { flavour = "moccha" } },
+
+  -- actually select the theme
+  { "LazyVim/LazyVim", opts = { colorscheme = "rose-pine" } },
+}
