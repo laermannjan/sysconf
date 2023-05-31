170c69d7e875d5defb7dcaaf3334b61c5b0e2ff2
Jan Laermann <laermannjan@gmail.com>
2023-05-31 20:48:35 +0200

    feat(nvim): fix cmp CR keymap

--- nvim/.config/nvim-lazyvim/lua/coding/cmp.lua
+++ nvim/.config/nvim-lazyvim/lua/coding/cmp.lua
@@ -3,11 +3,11 @@ return {
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
     local cmp = require("cmp")
-    table.insert(cmp.mapping.preset, {
-      ["<S-CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
-    })
-    table.insert(cmp.mapping.preset, {
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<Tab>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
+      ["<S-CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
+      ["<CR>"] = cmp.config.disable,
     })
   end,
 }
