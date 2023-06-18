1a92ac8db00c0794292165d8557ec5d05baacfc0
Jan Laermann <laermannjan@gmail.com>
2023-06-18 12:10:09 +0200

    feat(nvim): add beancount

---
+++ nvim/.config/nvim-lazyvim/lua/pde/beancount.lua
@@ -0,0 +1,37 @@
+return {
+  { "polarmutex/beancount.nvim" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "beancount" })
+      end
+    end,
+  },
+
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      table.insert(opts.sources, nls.builtins.formatting.bean_format)
+    end,
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "beancount-language-server" })
+      end
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        beancount = {},
+      },
+    },
+  },
+}
