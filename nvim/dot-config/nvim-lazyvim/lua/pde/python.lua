cd21eeb5cb42e6571c09933a3edcb98c8151cea7
Jan Laermann <laermannjan@gmail.com>
2023-07-02 18:26:57 +0200

    nvim: setup ruff auto-format, prefer local linter, formatter, etc

--- nvim/dot-config/nvim-lazyvim/lua/pde/python.lua
+++ nvim/dot-config/nvim-lazyvim/lua/pde/python.lua
@@ -1,3 +1,56 @@
+local lsp_path = require("lspconfig/util").path
+
+local function is_empty(s)
+  return s == nil or s == ""
+end
+
+local function get_pipenv_venv_path(workspace)
+  workspace = workspace or vim.fn.getcwd()
+
+  -- Find and use virtualenv from pipenv in workspace directory.
+  local match = vim.fn.glob(lsp_path.join(workspace, "Pipfile"))
+  if not is_empty(match) then
+    local venv = vim.fn.trim(vim.fn.system("PIPENV_PIPFILE=" .. match .. " pipenv --venv 2>/dev/null"))
+    require("notify")("Found pipenv venv", "debug", { title = "Python" })
+    return venv
+  end
+  return ""
+end
+
+local function get_peotry_venv_path(workspace)
+  workspace = workspace or vim.fn.getcwd()
+
+  -- Find and use virtualenv via poetry in workspace directory.
+  local match = vim.fn.glob(lsp_path.join(workspace, "poetry.lock"))
+  if not is_empty(match) then
+    local venv = vim.fn.trim(vim.fn.system("poetry env info -p 2>/dev/null"))
+    require("notify")("Found poetry venv", "debug", { title = "Python" })
+    return venv
+  end
+  return ""
+end
+
+local function get_local_venv_path(workspace)
+  local path = lsp_path.join(workspace, ".venv")
+  if not is_empty(vim.fn.glob(path)) then
+    require("notify")("Found local venv", "debug", { title = "Python" })
+    return lsp_path.dirname(path)
+  end
+
+  return ""
+end
+
+local function get_venv_path(workspace)
+  workspace = workspace or vim.fn.getcwd()
+  local venv = vim.VIRTUAL_ENV
+    or get_pipenv_venv_path(workspace)
+    or get_peotry_venv_path(workspace)
+    or get_local_venv_path(workspace)
+
+  require("notify")("Using  venv: " .. venv, "debug", { title = "Python" })
+  return venv
+end
+
 return {
   -- add python to treesitter
   {
@@ -13,8 +66,22 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      table.insert(opts.sources, nls.builtins.formatting.black)
-      table.insert(opts.sources, nls.builtins.diagnostics.mypy)
+      opts.sources = opts.sources or {}
+      vim.list_extend(opts.sources, {
+        -- Order of formatters matters. They are used in order of appearance.
+        nls.builtins.diagnostics.mypy.with({
+          filetypes = { "python" },
+          prefer_local = lsp_path.join(get_venv_path(), "bin"),
+        }),
+        nls.builtins.formatting.ruff.with({
+          filetypes = { "python" },
+          prefer_local = lsp_path.join(get_venv_path(), "bin"),
+        }),
+        nls.builtins.formatting.black.with({
+          filetypes = { "python" },
+          prefer_local = lsp_path.join(get_venv_path(), "bin"),
+        }),
+      })
     end,
   },
 
@@ -78,6 +145,7 @@ return {
         require("neotest-python")({
           dap = { justMyCode = false },
           runner = "pytest",
+          python = lsp_path.join(get_venv_path(), "bin", "python"),
         }),
       })
     end,
