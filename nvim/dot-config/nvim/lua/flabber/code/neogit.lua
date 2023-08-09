91cb241fec046b41f898138c43477e5214cdd7cc
Jan Laermann <laermannjan@gmail.com>
2023-08-09 15:23:37 +0200

    nvim fresh start

--- nvim/dot-config/nvim/lua/flabber/code/neogit.lua
+++
@@ -1,8 +0,0 @@
-return {
-    "TimUntersberger/neogit",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = true,
-    keys = {
-        { "<leader>gS", ":Neogit<CR>", desc = "Git status" },
-    },
-}
