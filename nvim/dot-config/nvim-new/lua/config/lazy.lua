e777b9c537f8b6ad192efe26159dae5ec83aa015
Jan Laermann <laermannjan@gmail.com>
2023-09-13 00:49:38 +0200

    style

--- nvim/dot-config/nvim-new/lua/config/lazy.lua
+++ nvim/dot-config/nvim-new/lua/config/lazy.lua
@@ -9,7 +9,7 @@ local lazy_opts = {
         size = { width = 0.8, height = 0.8 },
         wrap = true, -- wrap the lines in the ui
         -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
-        border = "double",
+        border = "rounded",
         colorscheme = { "tokyonight", "habamax" },
     },
 }
