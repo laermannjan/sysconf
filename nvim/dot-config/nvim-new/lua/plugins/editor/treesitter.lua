a1b10c0a2abd381a1421ee2d162554eed4977ef5
Jan Laermann <laermannjan@gmail.com>
2023-09-13 00:51:50 +0200

    add treesitter context objects

--- nvim/dot-config/nvim-new/lua/plugins/editor/treesitter.lua
+++ nvim/dot-config/nvim-new/lua/plugins/editor/treesitter.lua
@@ -1,31 +1,36 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    version = false, -- last release is way too old and doesn't work on Windows
-    build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-            "python",
-            "rust",
-            "markdown",
-            "markdown_inline",
-            "html",
-            "css",
-            "javascript",
-            "typescript",
-            "regex",
-            "lua",
-            "bash",
-            "fish",
-            "yaml",
-            "json",
-
-
+    {
+        "nvim-treesitter/nvim-treesitter",
+        version = false, -- last release is way too old and doesn't work on Windows
+        build = ":TSUpdate",
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = { 'org' },
+            },
+            indent = { enable = true },
+            ensure_installed = {
+                "python",
+                "rust",
+                "markdown",
+                "markdown_inline",
+                "html",
+                "css",
+                "javascript",
+                "typescript",
+                "regex",
+                "lua",
+                "bash",
+                "fish",
+                "yaml",
+                "json",
+                "org",
+            },
         },
+        config = function(_, opts)
+            require("nvim-treesitter.configs").setup(opts)
+        end
     },
-    config = function(_, opts)
-        require("nvim-treesitter.configs").setup(opts)
-    end
+    { "nvim-treesitter/nvim-treesitter-context", opts = {} }
 }
