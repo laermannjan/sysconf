c695b2c7551e4a2a32779e5c0777d91b70cc624a
Jan Laermann <laermannjan@gmail.com>
2023-09-13 00:50:06 +0200

    copilot.vim somehow works better

---
+++ nvim/dot-config/nvim-new/lua/plugins/coding/copilot.lua
@@ -0,0 +1,7 @@
+return { "github/copilot.vim", build = ":Copilot setup" }
+-- return {
+--     "zbirenbaum/copilot.lua",
+--     cmd = "Copilot",
+--     build = ":Copilot auth",
+--     opts = {}
+-- }
