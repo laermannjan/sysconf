61d41a91ba3bd1d00565f8de8c46e7039f7d943f
Jan Laermann <laermannjan@gmail.com>
2023-12-12 08:39:54 +0100

    new nvim and fish

--- fish/dot-config/fish/conf.d/fzf.fish
+++ fish/dot-config/fish/conf.d/fzf.fish
@@ -1,28 +1,6 @@
-# fzf.fish is only meant to be used in interactive mode. If not in interactive mode and not in CI, skip the config to speed up shell startup
-if not status is-interactive && test "$CI" != true
-    exit
-end
-
-# Because of scoping rules, to capture the shell variables exactly as they are, we must read
-# them before even executing _fzf_search_variables. We use psub to store the
-# variables' info in temporary files and pass in the filenames as arguments.
-# This variable is global so that it can be referenced by fzf_configure_bindings and in tests
-set --global _fzf_search_vars_command '_fzf_search_variables (set --show | psub) (set --names | psub)'
-
-
-# Install the default bindings, which are mnemonic and minimally conflict with fish's preset bindings
-fzf_configure_bindings
-
-# Doesn't erase autoloaded _fzf_* functions because they are not easily accessible once key bindings are erased
-function _fzf_uninstall --on-event fzf_uninstall
-    _fzf_uninstall_bindings
-
-    set --erase _fzf_search_vars_command
-    functions --erase _fzf_uninstall _fzf_migration_message _fzf_uninstall_bindings fzf_configure_bindings
-    complete --erase fzf_configure_bindings
-
-    set_color cyan
-    echo "fzf.fish uninstalled."
-    echo "You may need to manually remove fzf_configure_bindings from your config.fish if you were using custom key bindings."
-    set_color normal
-end
+set -gx FZF_DEFAULT_OPTS "
+	--color=fg:#908caa,bg:#191724,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:#191724
+	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
+	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
