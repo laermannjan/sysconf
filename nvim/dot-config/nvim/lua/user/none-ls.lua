a24375807afa7df3235674da051f218d0e22c750
Jan Laermann <laermannjan@gmail.com>
2024-01-05 16:27:10 +0100

    nvim: auto install null-ls sources

--- nvim/dot-config/nvim/lua/user/none-ls.lua
+++ nvim/dot-config/nvim/lua/user/none-ls.lua
@@ -1,31 +1,36 @@
 local M = {
 	"nvimtools/none-ls.nvim",
+	dependencies = { "jay-babu/mason-null-ls.nvim", "williamboman/mason.nvim" },
 }
 
 function M.config()
-	local null_ls = require("null-ls")
-
-	local formatting = null_ls.builtins.formatting
+	local null_ls = require "null-ls"
 
 	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-	null_ls.setup({
+	null_ls.setup {
 		debug = false,
 		on_attach = function(client, bufnr)
-			if client.supports_method("textDocument/formatting") then
-				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			if client.supports_method "textDocument/formatting" then
+				vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					group = augroup,
 					buffer = bufnr,
 					callback = function()
-						vim.lsp.buf.format({ async = false })
+						vim.lsp.buf.format { async = false }
 					end,
 				})
 			end
 		end,
 
 		sources = {
-			formatting.stylua,
-			formatting.prettier,
+			null_ls.builtins.formatting.stylua,
+			null_ls.builtins.formatting.prettier,
+
+			null_ls.builtins.code_actions.gomodifytags,
+			null_ls.builtins.code_actions.impl,
+			null_ls.builtins.formatting.goimports,
+			null_ls.builtins.formatting.gofumpt,
+
 			-- formatting.prettier.with {
 			--   extra_filetypes = { "toml" },
 			--   -- extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
@@ -34,7 +39,13 @@ function M.config()
 			-- null_ls.builtins.diagnostics.eslint,
 			null_ls.builtins.completion.spell,
 		},
-	})
+	}
+
+	require("mason-null-ls").setup {
+		---@diagnostic disable: assign-type-mismatch
+		ensure_installed = nil,
+		automatic_installation = true,
+	}
 end
 
 return M
