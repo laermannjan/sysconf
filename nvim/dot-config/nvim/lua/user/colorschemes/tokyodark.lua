9df0bb0fe6c4b93125889d18f6c2491dda4328b3
Jan Laermann <laermannjan@gmail.com>
2024-01-08 19:04:01 +0100

    nvim: add colorschemes

--- nvim/dot-config/nvim/lua/user/colorschemes/tokyodark.lua
+++ nvim/dot-config/nvim/lua/user/colorschemes/tokyodark.lua
@@ -1,12 +1,14 @@
 local M = {
 	"tiagovla/tokyodark.nvim",
+	lazy = false,
+	priority = 1000,
 	opts = {
-		-- custom options here
+		gamma = 1.2, -- adjust the brightness of the theme
 	},
 	config = function(_, opts)
 		if S.colorscheme == "tokyodark" then
 			require("tokyodark").setup(opts) -- calling setup is optional
-			vim.cmd [[colorscheme tokyodark]]
+			vim.cmd.colorscheme(S.colorscheme)
 		end
 	end,
 }
