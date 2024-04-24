fc9e8479a060cb885c63f9aee355e7e5afcc538c
Jan Laemrann <laermannjan@gmail.com>
2024-04-24 21:15:35 +0200

    add more darwin stuff

---
+++ modules/darwin/system.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  config = lib.mkIf pkgs.stdenv.isDarwin {
+    services.nix-daemon.enable = true;
+
+    # This setting only applies to Darwin, different on NixOS
+    nix.gc.interval = {
+      Hour = 12;
+      Minute = 15;
+      Day = 1;
+    };
+
+    environment.shells = [pkgs.fish];
+
+    security.pam.enableSudoTouchIdAuth = true;
+
+    system = {
+      keyboard = {
+        remapCapsLockToControl = true;
+        enableKeyMapping = true; # Allows for skhd
+      };
+    };
+  };
+}
