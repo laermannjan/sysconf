5c3359b5e88c830ed6d8d0ba05062f9b959641b4
Jan Laemrann <laermannjan@gmail.com>
2024-04-24 18:56:12 +0200

    add shell modules

---
+++ modules/common/shell/default.nix
@@ -0,0 +1,17 @@
+{...}: {
+  imports = [
+    # ./atuin.nix
+    # ./bash
+    # ./charm.nix
+    ./direnv
+    ./fish.nix
+    # ./fzf.nix
+    # ./git.nix
+    # ./github.nix
+    # ./jujutsu.nix
+    ./nixpkgs.nix
+    # ./starship.nix
+    # ./utilities.nix
+    # ./work.nix
+  ];
+}
---
+++ modules/common/shell/direnv/default.nix
@@ -0,0 +1,20 @@
+{config, ...}: {
+  # Enables quickly entering Nix shells when changing directories
+  home-manager.users.${config.user}.programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    config = {
+      whitelist = {
+        prefix = [config.sysconfPath];
+      };
+    };
+  };
+
+  xdg.configFile."direnv/lib/1password.sh".source = ./1password.sh;
+
+  # Prevent garbage collection
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
+}
