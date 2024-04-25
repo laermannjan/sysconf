1d4a08292a9a20ebbda5e9b10f0be1a779a1226c
Jan Laemrann <laermannjan@gmail.com>
2024-04-25 02:27:06 +0200

    fix fonts

--- modules/darwin/fonts.nix
+++ modules/darwin/fonts.nix
@@ -1,26 +1,27 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
-  home-manager.users.${config.user} = lib.mkIf pkgs.stdenv.isDarwin {
-    fonts = {
-      fontDir.enable = true;
-      fonts = with pkgs; [
-        (nerdfonts.override {
-          fonts = [
-            "Monaspace"
-            "Meslo"
-            "SourceCodePro"
-            "JetBrainsMono"
-            "Inconsolata"
-            "Hack"
-            "FiraCode"
-            "ComicShannsMono" # try to get ComicCodeMono instead
-          ];
-        })
+{pkgs, ...}: let
+  fonts = with pkgs; [
+    (nerdfonts.override {
+      fonts = [
+        "Monaspace"
+        "Meslo"
+        "SourceCodePro"
+        "JetBrainsMono"
+        "Inconsolata"
+        "Hack"
+        "FiraCode"
+        "ComicShannsMono" # try to get ComicCodeMono instead
       ];
+    })
+  ];
+in {
+  fonts =
+    if pkgs.stdenv.isDarwin
+    then {
+      fontDir.enable = true;
+      fonts = fonts;
+    }
+    else {
+      fontDir.enable = true;
+      packages = fonts;
     };
-  };
 }
