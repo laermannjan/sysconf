e666e9b3ee38ce9067154ac0569ee83be4967209
Jan Laemrann <laermannjan@gmail.com>
2024-04-25 02:34:18 +0200

    fix homebrw

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -1,38 +1,66 @@
-{...}: {
-  homebrew = {
-    # Homebrew Package Manager
-    enable = true;
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  # Homebrew - Mac-specific packages that aren't in Nix
+  config = lib.mkIf pkgs.stdenv.isDarwin {
+    # Requires Homebrew to be installed
+    system.activationScripts.preUserActivation.text = ''
+      if ! xcode-select --version 2>/dev/null; then
+        $DRY_RUN_CMD xcode-select --install
+      fi
+      if ! /opt/homebrew/bin/brew --version 2>/dev/null; then
+        $DRY_RUN_CMD /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+      fi
+    '';
 
-    onActivation = {
-      autoUpdate = false; # Don't update during rebuild # TODO: does `flake update` do this?
-      cleanup = "zap"; # Uninstall all programs not declared
-      upgrade = true;
+    # Add homebrew paths to CLI path
+    home-manager.users.${config.user}.home.sessionPath = ["/opt/homebrew/bin/"];
+
+    homebrew = {
+      # Homebrew Package Manager
+      enable = true;
+
+      onActivation = {
+        autoUpdate = false; # Don't update during rebuild # TODO: does `flake update` do this?
+        cleanup = "zap"; # Uninstall all programs not declared
+        upgrade = true;
+      };
+
+      global = {
+        brewfile = true; # Run brew bundle from anywhere
+        lockfiles = false; # Don't save lockfile (since running from anywhere)
+      };
+
+      brews = [];
+
+      taps = [
+        "homebrew/services"
+        "homebrew/cask-versions"
+      ];
+
+      # brews = [
+      #   "pinentry-mac"
+      #   {
+      #     name = "tor";
+      #     restart_service = true;
+      #   }
+      #   "torsocks"
+      # ];
+
+      casks = [
+        "1password" # 1Password will not launch from Nix on macOS
+        "karabiner-elements"
+        "mullvadvpn"
+        "rectangle"
+        "signal"
+        "tailscale"
+        # "thunderbird"
+        "docker"
+        # "firefox-developer-edition"
+      ];
     };
-
-    taps = [
-      "homebrew/services"
-      "homebrew/cask-versions"
-    ];
-
-    # brews = [
-    #   "pinentry-mac"
-    #   {
-    #     name = "tor";
-    #     restart_service = true;
-    #   }
-    #   "torsocks"
-    # ];
-
-    casks = [
-      "1password" # 1Password will not launch from Nix on macOS
-      "karabiner-elements"
-      "mullvadvpn"
-      "rectangle"
-      "signal"
-      "tailscale"
-      # "thunderbird"
-      "docker"
-      # "firefox-developer-edition"
-    ];
   };
 }
