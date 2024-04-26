23627f65b7dff34e00dbcb283dff31a4f9704de5
Jan Laemrann <laermannjan@gmail.com>
2024-04-26 20:22:47 +0200

    move fonts

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -6,6 +6,7 @@
 }: {
   imports = [
     ./applications
+    ./fonts.nix
     ./repositories
     ./shell
   ];
--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -6,7 +6,6 @@
 }: {
   imports = [
     ./fish.nix
-    ./fonts.nix
     ./homebrew.nix
     ./linearmouse
     ./networking.nix
