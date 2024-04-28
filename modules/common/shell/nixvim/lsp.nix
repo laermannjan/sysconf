fd4b471ca33c328742c9d579e2f7e9a45d57542a
Jan Laemrann <laermannjan@gmail.com>
2024-04-28 03:12:46 +0200

    fix lsp

--- modules/common/shell/nixvim/lsp.nix
+++ modules/common/shell/nixvim/lsp.nix
@@ -35,34 +35,33 @@
               lua = true;
             }
           ];
-
-          servers = {
-            bashls.enable = true;
-            beancount.enable = true;
-            gopls.enable = true;
-            helmls.enable = true;
-            jsonls.enable = true;
-            marksman.enable = true;
-            nil_ls.enable = true;
-            lua-ls.enable = true;
-            # pyright.enable = true;
-            pylsp = {
-              enable = true;
-              settings.plugins = {
-                pylsp_mypy.enabled = true;
-                ruff.enabled = true;
-              };
+        };
+        servers = {
+          bashls.enable = true;
+          beancount.enable = true;
+          gopls.enable = true;
+          helm-ls.enable = true;
+          jsonls.enable = true;
+          marksman.enable = true;
+          nil_ls.enable = true;
+          lua-ls.enable = true;
+          # pyright.enable = true;
+          pylsp = {
+            enable = true;
+            settings.plugins = {
+              pylsp_mypy.enabled = true;
+              ruff.enabled = true;
             };
-
-            # could be alternatives to the pylsp plugin
-            # ruff.enable = true;
-            # ruff-lsp.enable = true;
-            rust-analyzer.enable = true;
-            sqls.enable = true;
-            taplo.enable = true;
-            terraformls.enable = true;
-            yamlls.enable = true;
           };
+
+          # could be alternatives to the pylsp plugin
+          # ruff.enable = true;
+          # ruff-lsp.enable = true;
+          rust-analyzer.enable = true;
+          sqls.enable = true;
+          taplo.enable = true;
+          terraformls.enable = true;
+          yamlls.enable = true;
         };
       };
     };
