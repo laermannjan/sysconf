1e25becbd8ed1c9dd31c1ff838dfec9be818af30
Jan Laemrann <laermannjan@gmail.com>
2024-04-28 16:10:34 +0200

    add lsp icons

--- modules/common/shell/nixvim/lsp.nix
+++ modules/common/shell/nixvim/lsp.nix
@@ -7,6 +7,53 @@
       };
       lsp = {
         enable = true;
+        preConfig = ''
+          local icons = {
+              diagnostics = {
+                    BoldError = "",
+                    Error = "",
+                    BoldWarning = "",
+                    Warning = "",
+                    BoldInformation = "",
+                    Information = "",
+                    BoldQuestion = "",
+                    Question = "",
+                    BoldHint = "",
+                    Hint = "󰌶",
+                    Debug = "",
+                    Trace = "✎",
+                },
+            }
+
+          local default_diagnostic_config = {
+              signs = {
+                  active = true,
+                  values = {
+                      { name = "DiagnosticSignError", text = icons.diagnostics.Error },
+                      { name = "DiagnosticSignWarn", text = icons.diagnostics.Warning },
+                      { name = "DiagnosticSignHint", text = icons.diagnostics.Hint },
+                      { name = "DiagnosticSignInfo", text = icons.diagnostics.Information },
+                  },
+              },
+              virtual_text = false,
+              update_in_insert = false,
+              underline = true,
+              severity_sort = true,
+              float = {
+                  focusable = true,
+                  style = "minimal",
+                  border = "rounded",
+                  source = "always",
+                  header = "",
+                  prefix = "",
+              },
+          }
+
+          vim.diagnostic.config(default_diagnostic_config)
+          for _, sign in ipairs(vim.tbl_get(vim.diagnostic.config(), "signs", "values") or {}) do
+              vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = sign.name })
+          end
+        '';
 
         keymaps = {
           silent = true;
