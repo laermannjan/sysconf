c0c65943d2f67786f551f53e538b36e1c834ff34
Jan Laemrann <laermannjan@gmail.com>
2024-04-28 16:52:48 +0200

    update neotree binding

--- modules/common/shell/nixvim/neotree.nix
+++ modules/common/shell/nixvim/neotree.nix
@@ -1,4 +1,14 @@
 { ... }:
 {
-  programs.nixvim.plugins.neo-tree.enable = true;
+  programs.nixvim = {
+    plugins.neo-tree.enable = true;
+    keymaps = [
+      {
+        key = "<leader>e";
+        action = ":Neotree toggle reveal_force_cwd<cr>";
+        options.silent = true;
+        options.desc = "Toggle NeoTree";
+      }
+    ];
+  };
 }
