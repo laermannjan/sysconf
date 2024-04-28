9e548068385d807165920fe9690c02a58bbbd2a3
Jan Laemrann <laermannjan@gmail.com>
2024-04-28 14:07:45 +0200

    fix?

--- modules/common/shell/nixvim/telescope.nix
+++ modules/common/shell/nixvim/telescope.nix
@@ -17,43 +17,43 @@
           action = "find_files";
           options.desc = "find files";
         };
-        # "<leader>fw" = {
-        #   action = "grep_string";
-        #   options.desc = "find word/selection";
-        #   mode = [ "n" "v" ];
-        # };
-        # "<leader>fh" = {
-        #   action = "help_tags";
-        #   options.desc = "find help";
-        # };
-        # "<leader>fk" = {
-        #   action = "keymaps";
-        #   options.desc = "find help";
-        # };
-        # "<leader>fc" = {
-        #   action = "commands";
-        #   options.desc = "find command";
-        # };
-        # "<leader>fm" = {
-        #   action = "man_pages";
-        #   options.desc = "find man page";
-        # };
+        "<leader>fw" = {
+          action = "grep_string";
+          options.desc = "find word/selection";
+          mode = [ "n" "v" ];
+        };
+        "<leader>fh" = {
+          action = "help_tags";
+          options.desc = "find help";
+        };
+        "<leader>fk" = {
+          action = "keymaps";
+          options.desc = "find help";
+        };
+        "<leader>fc" = {
+          action = "commands";
+          options.desc = "find command";
+        };
+        "<leader>fm" = {
+          action = "man_pages";
+          options.desc = "find man page";
+        };
         # "<leader>uc" = {
         #   action = "colorscheme enable_preview=true";
         #   options.desc = "preview colorscheme";
         # };
-        # "<leader>\\" = {
-        #   action = "";
-        #   options.desc = "find telescope command";
-        # };
-        # "<leader>/" = {
-        #   action = "live_grep";
-        #   options.desc = "live grep";
-        # };
-        # "<leader><leader>" = {
-        #   action = "resume";
-        #   options.desc = "resume last search";
-        # };
+        "<leader>\\" = {
+          action = "";
+          options.desc = "find telescope command";
+        };
+        "<leader>/" = {
+          action = "live_grep";
+          options.desc = "live grep";
+        };
+        "<leader><leader>" = {
+          action = "resume";
+          options.desc = "resume last search";
+        };
       };
     };
   };
