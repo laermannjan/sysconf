b19dce89bdc161d577c4ef95039db264588c4803
Jan Laemrann <laermannjan@gmail.com>
2024-04-28 03:06:11 +0200

    add lsp

---
+++ modules/common/shell/nixvim/lsp.nix
@@ -0,0 +1,70 @@
+{
+  programs.nixvim = {
+    plugins = {
+      lspkind = {
+        enable = true;
+        cmp.enable = true;
+      };
+      lsp = {
+        enable = true;
+
+        keymaps = {
+          silent = true;
+          diagnostic = {
+            # Navigate in diagnostics
+            "<leader>k" = "goto_prev";
+            "<leader>j" = "goto_next";
+          };
+
+          lspBuf = {
+            gd = "definition";
+            gD = "declaration";
+            "<c-k>" = "signature_help";
+            gr = "references";
+            gt = "type_definition";
+            gi = "implementation";
+            K = "hover";
+            "<leader>cr" = "rename";
+            "<leader>ca" = "code_action";
+          };
+          extra = [
+            {
+              action = "vim.lsp.codelens.run";
+              key = "<leader>cl";
+              mode = "n";
+              lua = true;
+            }
+          ];
+
+          servers = {
+            bashls.enable = true;
+            beancount.enable = true;
+            gopls.enable = true;
+            helmls.enable = true;
+            jsonls.enable = true;
+            marksman.enable = true;
+            nil_ls.enable = true;
+            lua-ls.enable = true;
+            # pyright.enable = true;
+            pylsp = {
+              enable = true;
+              settings.plugins = {
+                pylsp_mypy.enabled = true;
+                ruff.enabled = true;
+              };
+            };
+
+            # could be alternatives to the pylsp plugin
+            # ruff.enable = true;
+            # ruff-lsp.enable = true;
+            rust-analyzer.enable = true;
+            sqls.enable = true;
+            taplo.enable = true;
+            terraformls.enable = true;
+            yamlls.enable = true;
+          };
+        };
+      };
+    };
+  };
+}
