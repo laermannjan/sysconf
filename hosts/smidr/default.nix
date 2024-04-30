d397e547eb44f3a80afb10efa6e295617ed7c39e
Jan Laermann <hello@mikgard.dev>
2024-04-30 17:35:18 +0200

    add yt music, tailscale gui, and fix pre-commit-hooks

--- hosts/smidr/default.nix
+++ hosts/smidr/default.nix
@@ -5,7 +5,7 @@
   overlays,
   ...
 }:
-inputs.darwin.lib.darwinSystem {
+inputs.nix-darwin.lib.darwinSystem {
   system = "aarch64-darwin";
   specialArgs = {
     inherit inputs;
@@ -29,7 +29,7 @@ inputs.darwin.lib.darwinSystem {
       _1password.enable = true;
       argo.enable = true;
       applauncher.enable = true;
-      brave.enable=true;
+      brave.enable = true;
       discord.enable = true;
       docker.enable = true;
       firefox.enable = true;
@@ -37,29 +37,30 @@ inputs.darwin.lib.darwinSystem {
       lua.enable = true;
       # neovim.enable = true;
       media.enable = true;
-      meetingbar.enable=true;
-      monitorcontrol.enable=true;
-      mullvad-browser.enable=true;
-      mullvad.enable=true;
+      meetingbar.enable = true;
+      monitorcontrol.enable = true;
+      mullvad-browser.enable = true;
+      mullvad.enable = true;
       nixlang.enable = true;
       nixvim.enable = true;
       notion-calendar.enable = true;
       notion.enable = true;
-      rectangle.enable=true;
+      rectangle.enable = true;
       obsidian.enable = true;
       postgres.enable = true;
       pycharm.enable = true;
       python.enable = true;
       rust.enable = true;
       slack.enable = true;
-      signal.enable=true;
+      signal.enable = true;
       stats.enable = true;
       sysconf.enable = true;
       tailscale.enable = true;
       terraform.enable = true;
-      tor-browser.enable=true;
-      utm.enable=true;
+      tor-browser.enable = true;
+      utm.enable = true;
       wezterm.enable = true;
+      youtube-music.enable = true;
       yt-dlp.enable = true;
     }
   ];
