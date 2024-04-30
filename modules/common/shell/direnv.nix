43a6d769ff8964a60c665b7442a463abd16aee00
Jan Laermann <hello@mikgard.dev>
2024-04-30 15:55:03 +0200

    less noisy direnv

--- modules/common/shell/direnv.nix
+++ modules/common/shell/direnv.nix
@@ -6,6 +6,10 @@
       enable = true;
       nix-direnv.enable = true;
       config = {
+        global = {
+            warn_timeout = 0;
+            hide_env_diff = true;
+        };
         whitelist = {
           prefix = [ config.sysconfPath ];
         };
