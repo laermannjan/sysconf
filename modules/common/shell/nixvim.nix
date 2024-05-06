f95ecd34d414b2ff0034a7b98593720ab67e0953
Jan Laermann <hello@mikgard.dev>
2024-05-06 12:17:53 +0200

    nixvim: actually add lazygit

--- modules/common/shell/nixvim.nix
+++ modules/common/shell/nixvim.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   inputs,
   config,
   lib,
@@ -20,28 +19,29 @@
     home-manager.users.${config.user} = {
       imports = [
         inputs.nixvim.homeManagerModules.nixvim
-        ./nixvim/colorscheme.nix
-        ./nixvim/options.nix
-        ./nixvim/keymaps.nix
-        ./nixvim/treesitter.nix
-        ./nixvim/lsp.nix
         ./nixvim/cmp.nix
-        ./nixvim/telescope.nix
-        ./nixvim/none-ls.nix
-        ./nixvim/smart-splits.nix
-        ./nixvim/neotest.nix
-        ./nixvim/dap.nix
+        ./nixvim/colorscheme.nix
         ./nixvim/comment.nix
-        ./nixvim/neotree.nix
-        ./nixvim/todo-comments.nix
-        ./nixvim/lualine.nix
-        ./nixvim/indent-blankline.nix
-        ./nixvim/nvim-autopair.nix
+        ./nixvim/dap.nix
         ./nixvim/diffview.nix
         ./nixvim/direnv.nix
         ./nixvim/git.nix
         ./nixvim/headlines.nix
         ./nixvim/hmts.nix
+        ./nixvim/indent-blankline.nix
+        ./nixvim/keymaps.nix
+        ./nixvim/lazygit.nix
+        ./nixvim/lsp.nix
+        ./nixvim/lualine.nix
+        ./nixvim/neotest.nix
+        ./nixvim/neotree.nix
+        ./nixvim/none-ls.nix
+        ./nixvim/nvim-autopair.nix
+        ./nixvim/options.nix
+        ./nixvim/smart-splits.nix
+        ./nixvim/telescope.nix
+        ./nixvim/todo-comments.nix
+        ./nixvim/treesitter.nix
         ./nixvim/trouble.nix
         ./nixvim/which-key.nix
       ];
