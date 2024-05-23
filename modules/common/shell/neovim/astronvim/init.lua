87021ce6dce1705f68d0dcc7628ceb97b6888149
Jan Laermann <hello@mikgard.dev>
2024-05-23 14:49:32 +0200

    update

---
+++ modules/common/shell/neovim/astronvim/init.lua
@@ -0,0 +1,49 @@
+-- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
+-- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- validate that lazy is available
+if not pcall(require, "lazy") then
+  -- stylua: ignore
+  vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+  vim.fn.getchar()
+  vim.cmd.quit()
+end
+
+require("lazy").setup({
+  {
+    "AstroNvim/AstroNvim",
+    version = "^4", -- Remove version tracking to elect for nighly AstroNvim
+    import = "astronvim.plugins",
+    opts = { -- AstroNvim options must be set here with the `import` key
+      mapleader = " ", -- This ensures the leader key must be configured before Lazy is set up
+      maplocalleader = ",", -- This ensures the localleader key must be configured before Lazy is set up
+      icons_enabled = true, -- Set to false to disable icons (if no Nerd Font is available)
+      pin_plugins = nil, -- Default will pin plugins when tracking `version` of AstroNvim, set to true/false to override
+    },
+  },
+  { import = "astro-core-plugins" },
+  { import = "astro-community-plugins" },
+  { import = "plugins" },
+} --[[@as LazySpec]], {
+  -- Configure any other `lazy.nvim` configuration options here
+  install = { colorscheme = { "astrodark", "habamax" } },
+  ui = { backdrop = 100 },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins, add more to your liking
+      disabled_plugins = {
+        "gzip",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "zipPlugin",
+      },
+    },
+  },
+} --[[@as LazyConfig]])
---
+++ modules/common/shell/neovim/astronvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,23 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  dependencies = {
+    {
+      "AstroNvim/astrocore",
+      opts = function(_, opts)
+        local maps = opts.mappings
+        maps.n["<C-H>"] = { function() require("smart-splits").move_cursor_left() end, desc = "Move to left split" }
+        maps.n["<C-J>"] = { function() require("smart-splits").move_cursor_down() end, desc = "Move to below split" }
+        maps.n["<C-K>"] = { function() require("smart-splits").move_cursor_up() end, desc = "Move to above split" }
+        maps.n["<C-L>"] = { function() require("smart-splits").move_cursor_right() end, desc = "Move to right split" }
+        maps.n["<C-Up>"] = false
+        maps.n["<C-Down>"] = false
+        maps.n["<C-Left>"] = false
+        maps.n["<C-Right>"] = false
+        maps.n["<C-S-H>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" }
+        maps.n["<C-S-J>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" }
+        maps.n["<C-S-K>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" }
+        maps.n["<C-S-L>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" }
+      end,
+    },
+  },
+}
