b05f57a06518c515081cb8d5524ba77f4834a8ca
Jan Laermann <hello@mikgard.dev>
2024-05-29 10:48:03 +0200

    let's manage lsp installs with nix

--- modules/common/programming/rust.nix
+++ modules/common/programming/rust.nix
@@ -8,24 +8,14 @@
   options.rust.enable = lib.mkEnableOption "Rust programming language.";
 
   config = lib.mkIf config.rust.enable {
-    home-manager.users.${config.user} =
-      let
-        libPath = with pkgs; lib.makeLibraryPath [ libiconv ];
-      in
-      {
-        home.packages = with pkgs; [
-          cargo
-          rustc
-          rust-analyzer
-          clippy
-          gcc
-          libiconv
-          llvmPackages.bintools
-        ];
-
-        home.sessionVariables = {
-          LD_LIBRARY_PATH = libPath;
-        };
-      };
+    home-manager.users.${config.user} = {
+      home.packages = with pkgs; [
+        cargo
+        rustc
+        rust-analyzer
+        clippy
+        gcc
+      ];
+    };
   };
 }
