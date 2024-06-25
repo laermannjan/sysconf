7bfa0783c848ff910a88c454b6886e468040c05e
Jan Laermann <hello@mikgard.dev>
2024-06-25 15:26:34 +0200

    install gcc with brew instead

--- modules/common/programming/python.nix
+++ modules/common/programming/python.nix
@@ -8,6 +8,11 @@
   options.python.enable = lib.mkEnableOption "Python programming language.";
 
   config = lib.mkIf config.python.enable {
+    homebrew.brews = [
+      "gcc"
+      "readline"
+      "openssl"
+    ];
     home-manager.users.${config.user} = {
       home.packages = with pkgs; [
         (python3.withPackages (
--- modules/common/programming/rust.nix
+++ modules/common/programming/rust.nix
@@ -8,11 +8,13 @@
   options.rust.enable = lib.mkEnableOption "Rust programming language.";
 
   config = lib.mkIf config.rust.enable {
+    homebrew.brews = [
+      "gcc"
+      "readline"
+      "openssl"
+    ];
     home-manager.users.${config.user} = {
-      home.packages = with pkgs; [
-        rustup
-        gcc
-      ];
+      home.packages = with pkgs; [ rustup ];
     };
   };
 }
