ade73de7f9f0e8d45267f41c2223125d9c8b5e2b
Jan Laermann <hello@mikgard.dev>
2024-08-07 19:05:44 +0200

    remove obsolete plugins, add autoformat

--- modules/common/shell/neovim/custom/lua/plugins/notify.lua
+++ modules/common/shell/neovim/custom/lua/plugins/notify.lua
@@ -1,9 +1,9 @@
 return {
-    "rcarriga/nvim-notify",
-    config = function ()
-        vim.notify = require("notify")
-        local ok, _ = pcall(require, 'telescope')
-        local rhs = ok and ":Telescope notify<cr>" or ":Notifications<cr>"
-        vim.keymap.set("n", "<leader>fn", rhs, { desc = "find notifications"})
-    end
+   "rcarriga/nvim-notify",
+   config = function()
+      vim.notify = require "notify"
+      local ok, _ = pcall(require, "telescope")
+      local rhs = ok and ":Telescope notify<cr>" or ":Notifications<cr>"
+      vim.keymap.set("n", "<leader>fn", rhs, { desc = "find notifications" })
+   end,
 }
