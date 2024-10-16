50fd83bb63bc19ba3a55388bec82c077ed2bbd5b
Jan Laermann <hello@mikgard.dev>
2024-10-16 16:45:29 +0200

    add newer less, nixfmt to neovim

--- modules/common/shell/neovim/custom/src/plugins/conform.lua
+++ modules/common/shell/neovim/custom/src/plugins/conform.lua
@@ -5,10 +5,14 @@ require('conform').setup({
         javascript = { 'prettier' },
         json = { 'prettier' },
         go = { 'goimports', 'gofumpt' },
+        nix = { 'nixfmt' },
     },
     format_on_save = {
         -- I recommend these options. See :help conform.format for details.
         lsp_format = 'fallback',
         timeout_ms = 500,
+    },
+    default_format_opts = {
+        lsp_format = 'fallback',
     },
 })
