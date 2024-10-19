dbfdfdc13f4fa1684559db5d137a57d9a7637466
jan <git@flabber.mozmail.com>
2024-10-19 22:18:09 +0200

    only add rust homebrew deps on darwin

--- modules/darwin/programming/rust.nix
+++ modules/darwin/programming/rust.nix
@@ -5,9 +5,12 @@
   ...
 }:
 {
-  options.rust.enable = lib.mkEnableOption "Rust programming language.";
-
   config = lib.mkIf config.rust.enable {
+    homebrew.brews = [
+      "gcc"
+      "readline"
+      "openssl"
+    ];
     home-manager.users.${config.user} = {
       home.packages = with pkgs; [ rustup ];
     };
