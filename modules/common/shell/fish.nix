36ab756c67c935cb97f77606dbfe8c939ce33aaf
Jan Laermann <hello@mikgard.dev>
2024-10-20 05:35:15 +0200

    fix fish path

--- modules/common/shell/fish.nix
+++ modules/common/shell/fish.nix
@@ -24,20 +24,33 @@
         secrets = "aws secretsmanager get-secret-value --secret-id (aws secretsmanager list-secrets | jq -r '.[][] | .Name' | fzf) | jq -r .SecretString | tr -d '\\n' | pbcopy";
         ssh-reset-alcemy = "ssh-keygen -R alhambra-dev.alcemy.tech && ssh-keygen -R alhambra-prod.alcemy.tech";
       };
+
+      # FIXME: This is needed to address bug where the $PATH is re-ordered by
+      # the `path_helper` tool, prioritising Apple’s tools over the ones we’ve
+      # installed with nix.
+      #
+      # This gist explains the issue in more detail: https://gist.github.com/Linerre/f11ad4a6a934dcf01ee8415c9457e7b2
+      # There is also an issue open for nix-darwin: https://github.com/LnL7/nix-darwin/issues/122
       loginShellInit =
         let
-          # This naive quoting is good enough in this case. There shouldn't be any
-          # double quotes in the input string, and it needs to be double quoted in case
-          # it contains a space (which is unlikely!)
-          dquote = str: ''"'' + str + ''"'';
+          # We should probably use `config.environment.profiles`, as described in
+          # https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
+          # but this takes into account the new XDG paths used when the nix
+          # configuration has `use-xdg-base-directories` enabled. See:
+          # https://github.com/LnL7/nix-darwin/issues/947 for more information.
+          profiles = [
+            "/etc/profiles/per-user/$USER" # Home manager packages
+            "$HOME/.nix-profile"
+            "(set -q XDG_STATE_HOME; and echo $XDG_STATE_HOME; or echo $HOME/.local/state)/nix/profile"
+            "/run/current-system/sw"
+            "/nix/var/nix/profiles/default"
+          ];
 
-          makeBinPathList = map (path: path + "/bin");
+          makeBinSearchPath = lib.concatMapStringsSep " " (path: "${path}/bin");
         in
-        # due to fish's weird sourcing order the nix paths won't be in front of the /usr/bin etc. paths, this fixes this
         ''
-          fish_add_path --move --prepend --path ${
-            lib.concatMapStringsSep " " dquote (makeBinPathList config.environment.profiles)
-          }
+          # Fix path that was re-ordered by Apple's path_helper
+          fish_add_path --move --prepend --path ${makeBinSearchPath profiles}
           set fish_user_paths $fish_user_paths
         '';
 
