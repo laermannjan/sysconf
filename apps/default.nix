2cedb76ac0a102c8908490ddba7d15a4119f5c6e
Jan Laermann <hello@mikgard.dev>
2024-10-23 11:41:56 +0200

    add loadkey app

--- apps/default.nix
+++ apps/default.nix
@@ -15,9 +15,9 @@ rec {
   # Rebuild
   rebuild = import ./rebuild.nix { inherit pkgs; };
 
-  # # Load the SSH key for this machine
-  # loadkey = import ./loadkey.nix { inherit pkgs; };
-  #
+  # Load the SSH key for this machine
+  loadkey = import ./loadkey.nix { inherit pkgs; };
+
   # # Encrypt secret for all machines
   # encrypt-secret = import ./encrypt-secret.nix { inherit pkgs; };
   #
