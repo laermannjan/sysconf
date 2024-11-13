315eb476448b4687c4f62b768e3575fd3d30520c
Jan Laermann <hello@mikgard.dev>
2024-11-14 00:14:16 +0100

    add test and debug mappings

--- modules/common/shell/neovim/custom/src/keymaps.lua
+++ modules/common/shell/neovim/custom/src/keymaps.lua
@@ -48,20 +48,25 @@ keymap('n', ']e', '<cmd>lua vim.diagnostic.goto_next({severity = "ERROR"})<cr>')
 
 _G.Config.leader_group_clues = {
     { mode = 'n', keys = '<Leader>b', desc = '+Buffer' },
+    { mode = 'n', keys = '<Leader>d', desc = '+Debug' },
     { mode = 'n', keys = '<Leader>e', desc = '+Explore' },
     { mode = 'n', keys = '<Leader>f', desc = '+Find' },
     { mode = 'n', keys = '<Leader>g', desc = '+Git' },
-    { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
+    -- { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
     { mode = 'n', keys = '<Leader>L', desc = '+Lua' },
     { mode = 'n', keys = '<Leader>o', desc = '+Other' },
-    { mode = 'n', keys = '<Leader>r', desc = '+R' },
-    { mode = 'n', keys = '<Leader>t', desc = '+Terminal/Minitest' },
-    { mode = 'n', keys = '<Leader>T', desc = '+Test' },
+    { mode = 'n', keys = '<Leader>t', desc = '+Tests' },
     { mode = 'n', keys = '<Leader>v', desc = '+Visits' },
 
-    { mode = 'x', keys = '<Leader>l', desc = '+LSP' },
-    { mode = 'x', keys = '<Leader>r', desc = '+R' },
+    -- { mode = 'x', keys = '<Leader>l', desc = '+LSP' },
+    -- { mode = 'x', keys = '<Leader>r', desc = '+R' },
 }
+
+local function get_args()
+    local input = vim.fn.input('Run with args (space separated, use `=`): ')
+    local args = vim.split(input, '%s+', { trimempty = true })
+    return args
+end
 
 -- b is for 'buffer'
 nmap_leader('ba', '<Cmd>b#<CR>', 'Alternate')
@@ -70,6 +75,25 @@ nmap_leader('bD', '<Cmd>lua MiniBufremove.delete(0, true)<CR>', 'Delete!')
 nmap_leader('bs', '<Cmd>lua Config.new_scratch_buffer()<CR>', 'Scratch')
 nmap_leader('bw', '<Cmd>lua MiniBufremove.wipeout()<CR>', 'Wipeout')
 nmap_leader('bW', '<Cmd>lua MiniBufremove.wipeout(0, true)<CR>', 'Wipeout!')
+
+-- d is for 'debug'
+nmap_leader('dB', function() require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: ')) end, 'Breakpoint Condition')
+nmap_leader('db', function() require('dap').toggle_breakpoint() end, 'Toggle Breakpoint')
+nmap_leader('dc', function() require('dap').continue() end, 'Run/Continue')
+nmap_leader('da', function() require('dap').continue({ before = get_args }) end, 'Run with Args')
+nmap_leader('dC', function() require('dap').run_to_cursor() end, 'Run to Cursor')
+nmap_leader('dg', function() require('dap').goto_() end, 'Go to Line (No Execute)')
+nmap_leader('di', function() require('dap').step_into() end, 'Step Into')
+nmap_leader('dj', function() require('dap').down() end, 'Down')
+nmap_leader('dk', function() require('dap').up() end, 'Up')
+nmap_leader('dl', function() require('dap').run_last() end, 'Run Last')
+nmap_leader('do', function() require('dap').step_out() end, 'Step Out')
+nmap_leader('dO', function() require('dap').step_over() end, 'Step Over')
+nmap_leader('dp', function() require('dap').pause() end, 'Pause')
+nmap_leader('dr', function() require('dap').repl.toggle() end, 'Toggle REPL')
+nmap_leader('ds', function() require('dap').session() end, 'Session')
+nmap_leader('dt', function() require('dap').terminate() end, 'Terminate')
+nmap_leader('dw', function() require('dap.ui.widgets').hover() end, 'Widgets')
 
 -- e is for 'explore' and 'edit'
 nmap_leader('ec', '<Cmd>lua MiniFiles.open(vim.fn.stdpath("config"))<CR>', 'Config')
@@ -131,15 +155,21 @@ nmap_leader('Lx', '<Cmd>lua Config.execute_lua_line()<CR>', 'Execute `lua` line'
 -- nmap_leader('tf', '<cmd>Neotest run vim.fn.expand("%")<cr>', 'Run file')
 -- nmap_leader('tt', '<cmd>Neotest run<cr>', 'Run nearest test')
 -- nmap_leader('ta', '<cmd>Neotest run suite=true<cr>', 'Run all tests')
-nmap_leader('tf', function() require('neotest').run.run(vim.fn.expand('%')) end, 'Run file')
-nmap_leader('tt', function() require('neotest').run.run() end, 'Run nearest')
-nmap_leader('ta', function() require('neotest').run.run({ suite = true }) end, 'Run all tests')
-nmap_leader('tr', function()
-    local prompt = 'test runner args (space separated, use `=`): '
-    local input = vim.fn.input(prompt)
-    local args = vim.split(input, '%s+', { trimempty = true })
-    require('neotest').run.run({ vim.fn.getcwd(), extra_args = args })
-end, 'Run tests in project (with args)')
+-- nmap_leader('tf', function() require('neotest').run.run(vim.fn.expand('%')) end, 'Run file')
+-- nmap_leader('tt', function() require('neotest').run.run() end, 'Run nearest')
+-- nmap_leader('ta', function() require('neotest').run.run({ suite = true }) end, 'Run all tests')
+-- nmap_leader('tr', function() require('neotest').run.run({ vim.fn.getcwd(), extra_args = args }) end, 'Run tests in project (with args)')
+
+nmap_leader('tf', function() require('neotest').run.run(vim.fn.expand('%')) end, 'Run File (Neotest)')
+nmap_leader('ta', function() require('neotest').run.run(vim.uv.cwd()) end, 'Run All Test Files (Neotest)')
+nmap_leader('tt', function() require('neotest').run.run() end, 'Run Nearest (Neotest)')
+nmap_leader('tT', function() require('neotest').run.run({ vim.uv.cwd(), extra_args = get_args() }) end, 'Run with args (Neotest)')
+nmap_leader('tl', function() require('neotest').run.run_last() end, 'Run Last (Neotest)')
+nmap_leader('ts', function() require('neotest').summary.toggle() end, 'Toggle Summary (Neotest)')
+nmap_leader('to', function() require('neotest').output.open({ enter = true, auto_close = true }) end, 'Show Output (Neotest)')
+nmap_leader('tO', function() require('neotest').output_panel.toggle() end, 'Toggle Output Panel (Neotest)')
+nmap_leader('tS', function() require('neotest').run.stop() end, 'Stop (Neotest)')
+nmap_leader('tw', function() require('neotest').watch.toggle(vim.fn.expand('%')) end, 'Toggle Watch (Neotest)')
 
 nmap_leader('vv', '<cmd>lua MiniVisits.add_label("tagged")<cr>', 'Add "tagged" label')
 nmap_leader('vd', '<cmd>lua MiniVisits.remove_label("tagged")<cr>', 'Remove "tagged" label')
