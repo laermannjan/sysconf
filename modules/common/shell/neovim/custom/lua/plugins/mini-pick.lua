3ce9b6917ee6884bd1211d2df3fbde293de48fee
Jan Laermann <hello@mikgard.dev>
2024-11-29 20:34:13 +0100

    add pick keymaps

--- modules/common/shell/neovim/custom/lua/plugins/mini-pick.lua
+++ modules/common/shell/neovim/custom/lua/plugins/mini-pick.lua
@@ -1,5 +1,12 @@
 return {
     'echasnovski/mini.pick',
+    keys = {
+        { '<leader>/', '<cmd>Pick grep_live<cr>', 'Grep' },
+        { '<leader>fb', '<cmd>Pick buffers<cr>', 'Buffers' },
+        { '<leader>ff', '<cmd>Pick files<cr>', 'Files' },
+        { '<leader>fh', '<cmd>Pick help"<cr>', 'Helptags' },
+        { '<leader>fw', '<cmd>Pick grep pattern="<cword>"<cr>', 'Grep word under cursor' },
+    },
     opts = true,
     config = function(opts)
         require('mini.pick').setup(opts)
