d120df56b1f92f01c6752f1e53cc3255493491c7
Jan Laermann <hello@mikgard.dev>
2024-11-30 16:29:56 +0100

    fix keys

--- modules/common/shell/neovim/custom/lua/plugins/mini-pick.lua
+++ modules/common/shell/neovim/custom/lua/plugins/mini-pick.lua
@@ -1,11 +1,11 @@
 return {
     'echasnovski/mini.pick',
     keys = {
-        { '<leader>/', '<cmd>Pick grep_live<cr>', 'Grep' },
-        { '<leader>fb', '<cmd>Pick buffers<cr>', 'Buffers' },
-        { '<leader>ff', '<cmd>Pick files<cr>', 'Files' },
-        { '<leader>fh', '<cmd>Pick help"<cr>', 'Helptags' },
-        { '<leader>fw', '<cmd>Pick grep pattern="<cword>"<cr>', 'Grep word under cursor' },
+        { '<leader>/', '<cmd>Pick grep_live<cr>', desc = 'Grep' },
+        { '<leader>fb', '<cmd>Pick buffers<cr>', desc = 'Buffers' },
+        { '<leader>ff', '<cmd>Pick files<cr>', desc = 'Files' },
+        { '<leader>fh', '<cmd>Pick help<cr>', desc = 'Helptags' },
+        { '<leader>fw', '<cmd>Pick grep pattern="<cword>"<cr>', desc = 'Grep word under cursor' },
     },
     opts = true,
     config = function(_, opts)
