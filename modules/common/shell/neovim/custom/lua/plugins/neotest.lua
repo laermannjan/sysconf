6e4fc817ff5c11732f81d5f25e12feaec4b2fbb2
Jan Laermann <hello@mikgard.dev>
2024-12-01 02:50:30 +0100

    test

--- modules/common/shell/neovim/custom/lua/plugins/neotest.lua
+++ modules/common/shell/neovim/custom/lua/plugins/neotest.lua
@@ -1,12 +1,27 @@
 return {
     {
         'nvim-neotest/neotest',
-        dependencies = { 'nvim-neotest/nvim-nio', 'nvim-lua/plenary.nvim' },
-        opts = {
-            adapters = {},
-            status = { virtual_text = true },
-            output = { open_on_run = true },
+        dependencies = {
+            'nvim-neotest/nvim-nio',
+            'nvim-lua/plenary.nvim',
+            'nvim-neotest/neotest-python',
         },
+        opts = {
+            -- adapters = {
+            --     require('neotest-python')({
+            --         -- dap = { justMyCode = false },
+            --     }),
+            -- },
+            -- status = { virtual_text = true },
+            -- output = { open_on_run = true },
+        },
+        config = function(_, opts)
+            require('neotest').setup({
+                adapters = {
+                    require('neotest-python')({}),
+                },
+            })
+        end,
         keys = {
             { '<leader>t', '', desc = '+test' },
             { '<leader>ta', function() require('neotest').run.run(vim.fn.expand('%')) end, desc = 'All (file)' },
@@ -19,50 +34,12 @@ return {
             { '<leader>tx', function() require('neotest').run.stop() end, desc = 'Stop' },
             { '<leader>tw', function() require('neotest').watch.toggle(vim.fn.expand('%')) end, desc = 'Toggle Watch' },
         },
-        config = function(_, opts)
-            local adapters = {}
-            if opts.adapters then
-                for name, config in ipairs(opts.adapters or {}) do
-                    local adapter = require(name)
-                    if type(config) == 'table' and not vim.tbl_isempty(config) then
-                        local meta = getmetatable(adapter)
-                        if adapter.setup then
-                            adapter.setup(config)
-                        elseif adapter.adapter then
-                            adapter.adapter(config)
-                            adapter = adapter.adapter
-                        elseif meta and meta.__call then
-                            adapter = adapter(config)
-                        else
-                            error('Adapter ' .. name .. ' does not support setup')
-                        end
-                    end
-
-                    adapters[#adapters + 1] = adapter
-                end
-            end
-            opts.adapters = adapters
-            require('neotest').setup(opts)
-        end,
     },
     {
         'mfussenegger/nvim-dap',
         optional = true,
         keys = {
             { '<leader>td', function() require('neotest').run.run({ strategy = 'dap' }) end, desc = 'Debug Nearest' },
-        },
-    },
-    {
-        'nvim-neotest/neotest',
-        dependencies = {
-            'nvim-neotest/neotest-python',
-        },
-        opts = {
-            adapters = {
-                ['neotest-python'] = {
-                    dap = { justMyCode = false },
-                },
-            },
         },
     },
 }
