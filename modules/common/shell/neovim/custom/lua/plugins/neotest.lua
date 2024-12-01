e3d81842527d53d6cbc7eb02919e4c5ca17ef4ff
Jan Laermann <hello@mikgard.dev>
2024-12-01 17:38:16 +0100

    refactor

--- modules/common/shell/neovim/custom/lua/plugins/neotest.lua
+++ modules/common/shell/neovim/custom/lua/plugins/neotest.lua
@@ -4,24 +4,53 @@ return {
         dependencies = {
             'nvim-neotest/nvim-nio',
             'nvim-lua/plenary.nvim',
-            'nvim-neotest/neotest-python',
             'nvim-treesitter/nvim-treesitter',
         },
         opts = {
-            -- adapters = {
-            --     require('neotest-python')({
-            --         -- dap = { justMyCode = false },
-            --     }),
-            -- },
-            -- status = { virtual_text = true },
-            -- output = { open_on_run = true },
+            adapters = {},
+            status = { virtual_text = true },
+            output = { open_on_run = true },
         },
         config = function(_, opts)
-            require('neotest').setup({
-                adapters = {
-                    require('neotest-python')({}),
+            local neotest_ns = vim.api.nvim_create_namespace('neotest')
+            vim.diagnostic.config({
+                virtual_text = {
+                    format = function(diagnostic)
+                        -- Replace newline and tab characters with space for more compact diagnostics
+                        local message = diagnostic.message:gsub('\n', ' '):gsub('\t', ' '):gsub('%s+', ' '):gsub('^%s+', '')
+                        return message
+                    end,
                 },
-            })
+            }, neotest_ns)
+
+            if opts.adapters then
+                local adapters = {}
+                for name, config in pairs(opts.adapters or {}) do
+                    if type(name) == 'number' then
+                        if type(config) == 'string' then config = require(config) end
+                        adapters[#adapters + 1] = config
+                    elseif config ~= false then
+                        local adapter = require(name)
+                        if type(config) == 'table' and not vim.tbl_isempty(config) then
+                            local meta = getmetatable(adapter)
+                            if adapter.setup then
+                                adapter.setup(config)
+                            elseif adapter.adapter then
+                                adapter.adapter(config)
+                                adapter = adapter.adapter
+                            elseif meta and meta.__call then
+                                adapter = adapter(config)
+                            else
+                                error('Adapter ' .. name .. ' does not support setup')
+                            end
+                        end
+                        adapters[#adapters + 1] = adapter
+                    end
+                end
+                opts.adapters = adapters
+            end
+
+            require('neotest').setup(opts)
         end,
         keys = {
             { '<leader>t', '', desc = '+test' },
@@ -41,6 +70,23 @@ return {
         optional = true,
         keys = {
             { '<leader>td', function() require('neotest').run.run({ strategy = 'dap' }) end, desc = 'Debug Nearest' },
+        },
+    },
+
+    {
+        'nvim-neotest/neotest',
+        optional = true,
+        dependencies = {
+            'nvim-neotest/neotest-python',
+        },
+        opts = {
+            adapters = {
+                ['neotest-python'] = {
+                    -- Here you can specify the settings for the adapter, i.e.
+                    -- runner = "pytest",
+                    -- python = ".venv/bin/python",
+                },
+            },
         },
     },
 }
