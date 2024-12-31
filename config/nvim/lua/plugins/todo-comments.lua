163bf576ffca537967f5c55b958f825acc12be95
Jan Laermann <git@flabber.mozmail.com>
2024-12-31 13:17:42 +0100

    fix todo-comments

--- config/nvim/lua/plugins/todo-comments.lua
+++ config/nvim/lua/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
     'folke/todo-comments.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
+    lazy = false,
     opts = {},
     keys = {
         { ']t', function() require('todo-comments').jump_next() end, desc = 'Next todo comment' },
