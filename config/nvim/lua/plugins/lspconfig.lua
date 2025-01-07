a701cdadfef132b2f0737b10f8f05ad430b7a6bf
Jan Laermann <git@flabber.mozmail.com>
2025-01-07 02:04:36 +0100

    add love2d

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -6,6 +6,7 @@ return {
         opts = {
             library = {
                 { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
+                { path = '${3rd}/love2d/library', words = { 'love' } },
                 { path = 'snacks.nvim', words = { 'Snacks' } },
                 { path = 'lazy.nvim', words = { 'LazyVim' } },
             },
