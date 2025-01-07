73da5efa327a016023fc10ba0350619d90afc9c5
Jan Laermann <git@flabber.mozmail.com>
2025-01-07 01:07:08 +0100

    update neovim opts

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -70,21 +70,20 @@ if vim.fn.exists('syntax_on') ~= 1 then vim.cmd('syntax enable') end
 
 -- Editing ====================================================================
 vim.o.autoindent    = true     -- Use auto indent
-vim.o.expandtab     = true     -- Convert tabs to spaces in insert mode
+-- vim.o.smartindent   = true     -- Make indenting smart
+vim.opt.cpoptions:append('I')  -- Don't delete indent from autoindent when not typing anything or moving away
+-- vim.o.expandtab     = true     -- Convert tabs to spaces in insert mode
 vim.o.formatoptions = 'rqnl1j' -- Improve comment editing
 vim.o.ignorecase    = true     -- Ignore case when searching (use `\C` to force not doing that)
+vim.o.smartcase     = true     -- Don't ignore case when searching if pattern has upper case
 vim.o.incsearch     = true     -- Show search results while typing
 vim.o.hlsearch      = true     -- Show search results even after hitting <CR>
-vim.o.infercase     = true     -- Infer letter cases for a richer built-in keyword completion
 vim.o.shiftwidth    = 4        -- Use this number of spaces for indentation
+vim.o.softtabstop   = -1       -- Negative value to use the same value as shiftwidth
 vim.o.shiftround    = true     -- Round indent to multiple of shiftwidth when using < and >
-vim.o.smartcase     = true     -- Don't ignore case when searching if pattern has upper case
-vim.o.smartindent   = true     -- Make indenting smart
-vim.o.smarttab      = true     -- Use shiftwidth (at beginning of line) or tabstop (otherwise) spaces for tab, remove them with a single backspace
-vim.o.tabstop       = 4        -- Insert 4 spaces for a tab
 vim.o.virtualedit   = 'block'  -- Allow going past the end of line in visual block mode
 
-vim.opt.iskeyword:append('-')  -- Treat dash separated words as a word text object
+-- vim.opt.iskeyword:append('-')  -- Treat dash separated words as a word text object
 
 -- Define pattern for a start of 'numbered' list. This is responsible for
 -- correct formatting of lists when using `gw`. This basically reads as 'at
@@ -103,7 +102,7 @@ vim.o.spelloptions = 'camel'      -- Treat parts of camelCase words as seprate w
 
 -- Folds ======================================================================
 vim.o.foldmethod  = 'indent' -- Set 'indent' folding method
-vim.o.foldlevel   = 2        -- Display all folds except top two
+vim.o.foldlevel   = 3        -- Display all folds except top two
 vim.o.foldnestmax = 10       -- Create folds only for some number of nested levels
 vim.g.markdown_folding = 1   -- Use folding by heading in markdown files
 
@@ -116,9 +115,11 @@ if vim.fn.has('nvim-0.11') == 1 then
 end
 
 -- Custom autocommands ========================================================
-local augroup = vim.api.nvim_create_augroup('CustomSettings', {})
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
 vim.api.nvim_create_autocmd('FileType', {
-  group = augroup,
+  group = augroup('formatoptions'),
   callback = function()
     -- Don't auto-wrap comments and don't insert comment leader after hitting 'o'
     -- If don't do this on `FileType`, this keeps reappearing due to being set in
@@ -127,4 +128,73 @@ vim.api.nvim_create_autocmd('FileType', {
   end,
   desc = [[Ensure proper 'formatoptions']],
 })
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup("highlight_yank"),
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+-- go to last loc when opening a buffer
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = augroup("last_loc"),
+  callback = function(event)
+    local exclude = { "gitcommit" }
+    local buf = event.buf
+    if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
+      return
+    end
+    vim.b[buf].lazyvim_last_loc = true
+    local mark = vim.api.nvim_buf_get_mark(buf, '"')
+    local lcount = vim.api.nvim_buf_line_count(buf)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+-- close some filetypes with <q>
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("close_with_q"),
+  pattern = {
+    "checkhealth",
+    "dbout",
+    "gitsigns-blame",
+    "grug-far",
+    "help",
+    "lspinfo",
+    "neotest-output",
+    "neotest-output-panel",
+    "neotest-summary",
+    "notify",
+  },
+  callback = function(event)
+    vim.bo[event.buf].buflisted = false
+    vim.schedule(function()
+      vim.keymap.set("n", "q", function()
+        vim.cmd("close")
+        pcall(vim.api.nvim_buf_delete, event.buf, { force = true })
+      end, {
+        buffer = event.buf,
+        silent = true,
+        desc = "Quit buffer",
+      })
+    end)
+  end,
+})
+
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup("auto_create_dir"),
+  callback = function(event)
+    if event.match:match("^%w%w+:[\\/][\\/]") then
+      return
+    end
+    local file = vim.uv.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
+  end,
+})
+
 --stylua: ignore end
