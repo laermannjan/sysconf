c8d244d836ba27c009f41a6aa8ddd06f6a316f37
Jan Laermann <git@flabber.mozmail.com>
2025-08-17 12:55:17 +0200

    fzf: make tokyonight colored

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -60,6 +60,11 @@ status is-interactive; and begin
     end
 
     if command -q fzf
+        # tokyonight-night
+        set -gx FZF_DEFAULT_OPTS "$FZF_DEFAULT_OPTS
+            --highlight-line --info=inline-right --ansi --layout=reverse --border=none
+            --color=bg+:#283457,bg:#16161e,border:#27a1b9,fg:#c0caf5,gutter:#16161e,header:#ff9e64,hl+:#2ac3de,hl:#2ac3de,info:#545c7e
+            --color=marker:#ff007c,pointer:#ff007c,prompt:#2ac3de,query:#c0caf5:regular,scrollbar:#27a1b9,separator:#ff9e64,spinner:#ff007c"
         fzf --fish | source
     end
 
