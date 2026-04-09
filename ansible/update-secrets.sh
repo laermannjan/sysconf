d8bb9fd62b8f1decf90739aea927ffdd1e6178b9
Jan Laermann <git@flabber.mozmail.com>
2026-04-09 15:34:45 +0200

    ansible(ssh): remove shared key vaults and update-secrets.sh

--- ansible/update-secrets.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-[ ! -f "${VAULT_PASSWORD_FILE}" ] && echo "ERROR: Ansible vault password file not found! Create one with the vault password and specify \$VAULT_PASSWORD_FILE" 1>&2 && exit 1
-
-for file in "~/.ssh/config" "~/.ssh/id_ed25519.alcemy" "~/.ssh/id_ed25519.alcemy.pub" "~/.ssh/id_ed25519.personal" "~/.ssh/id_ed25519.personal.pub"; do
-    src="${file/#~/$HOME}"
-    dest="${HOME}/sysconf/ansible/roles/ssh/files/$(basename "${src}").vault"
-    ansible-vault encrypt "${src}" --output ${dest} --vault-password-file "${VAULT_PASSWORD_FILE}"
-done
