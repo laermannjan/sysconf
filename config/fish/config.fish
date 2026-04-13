366e8ffae8c215c8babe96299ff22c9ce054fdb3
Jan Laermann <git@flabber.mozmail.com>
2026-04-13 12:25:23 +0200

    ansible(packages): manage via Brewfile as much as possible
    asdf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,6 +9,7 @@ test -f /home/linuxbrew/.linuxbrew/bin/brew; and set brew_prefix /home/linuxbrew
 test -f /opt/homebrew/bin/brew; and set brew_prefix /opt/homebrew
 if set -q brew_prefix
     set -gx HOMEBREW_NO_ANALYTICS 1
+    set -gx HOMEBREW_BUNDLE_FILE $XDG_CONFIG_HOME/homebrew/Brewfile
     $brew_prefix/bin/brew shellenv | source
     set -p fish_complete_path $brew_prefix/share/fish/completions
     set -p fish_complete_path $brew_prefix/share/fish/vendor_completions.d
