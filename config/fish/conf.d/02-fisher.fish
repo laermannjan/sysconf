5c45489962aaed2150f7bc174714b4646b742142
Jan Laermann <git@flabber.mozmail.com>
2026-04-17 02:22:01 +0200

    refactor(fish): bootstrap fisher via conf.d, remove ansible task

--- config/fish/conf.d/02-fisher.fish
+++ config/fish/conf.d/02-fisher.fish
@@ -4,7 +4,7 @@ set -p fish_function_path $fisher_path/functions
 set -p fish_complete_path $fisher_path/completions
 for f in $fisher_path/conf.d/*.fish; source $f; end
 
-if status is-interactive && not functions -q fisher
+if not functions -q fisher
     curl -sL https://git.io/fisher | source
     fisher update
 end
